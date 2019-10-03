5a4b8562e41db8a8bf6e4a50ef6593fe023bddee
amrwc <42574496+amrwc@users.noreply.github.com>
2019-10-03 20:41:34 +0100

    Set up the repository
    - add `.gitignore`,
    - add `.vscode`,
    - add `.prettierrc.js`.

---
+++ .prettierrc.js
@@ -0,0 +1,20 @@
+/**
+ * Prettier configuration. Place it in the parent directory of the relevant
+ * files, or in the root of a project if the configuration should be applied
+ * everywhere.
+ *
+ * @author amrwc
+ * @read https://prettier.io/docs/en/options.html
+ * @read https://prettier.io/docs/en/configuration.html
+ */
+module.exports = {
+	arrowParens: 'avoid',
+	bracketSpacing: true,
+	printWidth: 100,
+	quoteProps: 'as-needed',
+	semi: true,
+	singleQuote: true,
+	trailingComma: 'es5',
+	tabWidth: 4,
+	useTabs: true,
+};
