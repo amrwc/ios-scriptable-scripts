261d461c08ce3f05d8820c245c4b867a712e9dfa
amrwc <42574496+amrwc@users.noreply.github.com>
2020-01-07 12:16:15 +0100

    Add information to the 'Message Many' script

--- src/Message Many.js
+++ src/Message Many.js
@@ -3,6 +3,13 @@
 // icon-color: red; icon-glyph: envelope;
 // Message Many
 
+/**
+ * The script is peculiar in that it presents the Messages UI before with the contents and reci-
+ * pients filled in but doesn't send it right away – the user must manually press the send button.
+ *
+ * It can be modified to attach images/files as per the Message object documentation.
+ */
+
 const RECIPIENTS = [
 	// strings with phone numbers
 ];
